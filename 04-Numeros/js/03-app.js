1b1af148615fca262d910dcd0c52583291bc71ef
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-08-28 15:31:50 -0600

    feat (04-Numeros): ✨Section 04 the course has been completed

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,42 @@
+let resultado;
+
+//PI
+resultado= Math.PI;
+
+//Redondear
+resultado=Math.round(2.8);
+resultado=Math.round(2.2);
+resultado=Math.round(2.5);
+resultado=Math.round(2.6);
+
+//Redondear hacia arriba
+resultado=Math.ceil(2.1);
+
+//Redondear hacia abajo
+resultado=Math.floor(2.9);
+
+//Raiz Cuadrada
+resultado=Math.sqrt(14423322);
+
+//Absoluto
+resultado=Math.abs(-500);
+
+//Potencia
+resultado=Math.pow(8,3);
+
+//Minimo
+resultado=Math.min(3,5,1,12,-3);
+
+//Max
+resultado=Math.min(3,5,1,12,-3);
+
+//Minimo
+resultado=Math.min(3,5,1,12,-3);
+
+//Aleatorio
+resultado=Math.random();
+
+//Aleatorio dentro de un rango
+resultado=Math.floor(Math.random()*30);
+
+console.log(resultado);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,14 @@
+let puntaje=5;
+
+puntaje++; //5
+++puntaje; //6
+
+puntaje--;//5
+--puntaje;//4
+
+//toma los anteriores y suma
+puntaje+=3;
+
+puntaje-=3;
+
+console.log(puntaje);
