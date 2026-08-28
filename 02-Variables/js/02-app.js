32e025dc68d83df47522843036f4963730def2d2
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-08-28 12:17:10 -0600

    feat (02-Variables): ✨Section 02 the course has been completed

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,16 @@
+let producto = 'Tablet';
+
+//Reasiganr el valor  (se pueden reasignar las variables cuando esta definidas con let)
+
+producto = 'Monitor';
+
+producto= 20;
+producto= true;
+producto= null
+
+console.log(producto);
+
+let precio;
+precio = 300;
+
+console.log(precio)
---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,10 @@
+const producto = 'Tablet';
+
+// Const no puede ser reasignado
+//producto = 'Monitor';
+
+console.log(producto);
+
+//Deden inicializar con un valor
+const precio = 20;
+console.log(precio);
