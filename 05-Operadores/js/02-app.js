689cb94a64a35be77d7b2f890599a98138cabd99
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-08-28 15:54:11 -0600

    feat (05-Operadores): ✨Section 05 the course has been completed

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,22 @@
+const numero1=20;
+const numero2="20";
+const numero3=30;
+
+//Revisar si 2 números son iguales
+
+console.log(numero1 == numero3);
+console.log(numero1 == numero2);
+
+//Commparardor estrcicto
+console.log(numero1 === numero2);
+console.log(numero1 === parseInt(numero2));
+console.log(typeof numero1);
+console.log(typeof numero2);
+
+//Comparador diferente a 
+const password1 = 'admin1';
+const password2 = 'admin2';
+
+console.log(password1 != password2);
+console.log(numero1 != numero2);
+console.log(numero1 !== parseInt(numero2));
---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,13 @@
+//Undefinded
+let numero;
+
+console.log(numero);
+
+console.log(typeof numero);
+
+//Null
+let numero2= null;
+console.log(numero2);
+//console.log(typeof numero 2);
+
+console.log(numero === numero2 );
