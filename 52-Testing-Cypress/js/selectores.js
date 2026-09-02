74b1ecce9b5d60d062be9b417000944405025af6
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-02 15:25:54 -0600

    feat(Testing): ✨ CYPRESS TESTING

--- 52-Testing-Cypress/js/selectores.js
+++ 52-Testing-Cypress/js/selectores.js
@@ -5,6 +5,7 @@ export const fechaInput = document.querySelector('#fecha');
 export const horaInput = document.querySelector('#hora');
 export const sintomasInput = document.querySelector('#sintomas');
 
+
 // Contenedor para las citas
 export const contenedorCitas = document.querySelector('#citas');
 
